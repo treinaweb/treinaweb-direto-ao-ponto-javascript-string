011e18f333406b810c63f715130fe5f3ca0dc381
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-12 09:33:02 -0300

    01:02 - Como criar String primitivas

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const string2 = '50';
-const minhaString = `o valor máximo é: ${string2}
-    mas eu só tenho ${30 + 5}
-`;
 
-console.log(minhaString);
+console.log('o fulano falo: "..."');
+
+console.log("caixa d'água");
